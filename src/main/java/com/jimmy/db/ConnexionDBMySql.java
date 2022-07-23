dfe05372bf8986cc7540476f09d4328336ee7c9c
Jimmy lepage <lepage.jimmy.martin@gmail.com>
2022-07-23 06:22:48 +0200

    Ajout connexion en static dans ConnexionDBMySql

--- src/main/java/com/jimmy/db/ConnexionDBMySql.java
+++ src/main/java/com/jimmy/db/ConnexionDBMySql.java
@@ -7,30 +7,37 @@ import com.jimmy.util.Propriete;
 
 public class ConnexionDBMySql implements ConnexionDB {
 
+	private static Connection connexion;
+
 	public Connection ouvrirConnexion() {
 
-		Connection connexion;
-		try {
-			Class.forName("com.mysql.jdbc.Driver"); // Charge le driver dans la VM
-
-			// On recherche les paramètres de connexion à la DB MySQL
-
-			Propriete propriete = new Propriete();
-
-			String urlMySql = propriete.rechercheProprieteString("urlMySql");
-			String utilisateurMySql = propriete.rechercheProprieteString("utilisateurMySql");
-			String motDePasseMySql = propriete.rechercheProprieteString("motDePasseMySql");
-
-			connexion = DriverManager.getConnection(urlMySql, utilisateurMySql, motDePasseMySql);
-			System.out.println("Connexion établie sur " + urlMySql + " avec utilisateur " + utilisateurMySql);
+		if (connexion != null) {
 
 			return connexion;
 
-		} catch (Exception e) {
-			System.out.println(e);
+		} else {
+			try {
+				Class.forName("com.mysql.jdbc.Driver"); // Charge le driver dans la VM
+
+				// On recherche les paramètres de connexion à la DB MySQL
+
+				Propriete propriete = new Propriete();
+
+				String urlMySql = propriete.rechercheProprieteString("urlMySql");
+				String utilisateurMySql = propriete.rechercheProprieteString("utilisateurMySql");
+				String motDePasseMySql = propriete.rechercheProprieteString("motDePasseMySql");
+
+				connexion = DriverManager.getConnection(urlMySql, utilisateurMySql, motDePasseMySql);
+				System.out.println("Connexion établie sur " + urlMySql + " avec utilisateur " + utilisateurMySql);
+
+				return connexion;
+
+			} catch (Exception e) {
+				System.out.println(e);
+
+			}
 
 			return null;
-
 		}
 	}
 
