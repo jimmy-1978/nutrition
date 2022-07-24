3a84919b55d56e67cba014f0957fd9611594d81f
Jimmy lepage <lepage.jimmy.martin@gmail.com>
2022-07-24 14:09:10 +0200

    plus besoin de gérer la connexion dans UtilisateurDaoImplTest

--- src/test/java/com/jimmy/db/UtilisateurDaoImplTest.java
+++ src/test/java/com/jimmy/db/UtilisateurDaoImplTest.java
@@ -2,11 +2,7 @@ package com.jimmy.db;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.sql.Connection;
-
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -15,20 +11,7 @@ import com.jimmy.classes.Utilisateur;
 
 class UtilisateurDaoImplTest {
 
-	private static ConnexionDBMySql connexionDBMysql;
-	private static Connection connexion;
 	private UtilisateurDaoImpl utilisateurDaoImpl;
-
-	@BeforeAll
-	static void avantTousLesTest() {
-		connexionDBMysql = new ConnexionDBMySql();
-		connexion = connexionDBMysql.ouvrirConnexion();
-	}
-
-	@AfterAll
-	static void apresTousLesTests() {
-		connexionDBMysql.fermerConnexion(connexion);
-	}
 
 	@BeforeEach
 	void avantChaqueTest() {
